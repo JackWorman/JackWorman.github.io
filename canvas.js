4161cfe7db31f97ff7a542acb9bddb359abe54e8
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-06-08 22:02:29 -0500

    df

--- canvas.js
+++ canvas.js
@@ -2,8 +2,7 @@
 const CANVAS_SIZE = 750;
 
 // Globals
-canvas = document.getElementById('myCanvas');
-context = canvas.getContext('2d');
+
 var board;
 var nIntervId;
 
@@ -13,22 +12,24 @@ $(document).ready(function() {
 
   setUpCanvas();
 
-  // var board = new Array(50);
-  // for (var i = 0; i < 50; i++) {
-  //   board[i] = new Array(50);
-  //   for (var j = 0; j < 50; j++) {
-  //     board[i][j] = 0;
-  //   }
-  // }
-  // board[3][10] = 1;
+  var board = new Array(50);
+  for (var i = 0; i < 50; i++) {
+    board[i] = new Array(50);
+    for (var j = 0; j < 50; j++) {
+      board[i][j] = 0;
+    }
+  }
+  board[3][10] = 1;
 
-  //nIntervId = setInterval(test, 1000);
+  nIntervId = setInterval(test, 1000);
 });
 
 function setUpCanvas() {
+  var canvas = document.getElementById('myCanvas');
   canvas.width = CANVAS_SIZE;
   canvas.height = CANVAS_SIZE;
   // Draws a grid onto the canvas.
+  var context = canvas.getContext('2d');
   for (var i = 1; i < 50; i++) {
     var step = i * CANVAS_SIZE / 50;
     context.moveTo(0, step);
@@ -41,6 +42,7 @@ function setUpCanvas() {
 }
 
 function test() {
+  var context = document.getElementById('myCanvas').getContext('2d');
   context.fillStyle = "#FF0000";
   for (var i = 0; i < 50; i++) {
     for (var j = 0; j < 50; j++) {

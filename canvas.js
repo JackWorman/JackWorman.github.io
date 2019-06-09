9bcafd39b15dfee38739e80a9a31fd7a3ab1d37f
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-06-08 23:07:45 -0500

    moving box

--- canvas.js
+++ canvas.js
@@ -9,21 +9,26 @@ var nIntervId;
 var canvas;
 var context;
 
+var direction = 0; // 0 = none, 1 = right, 2 = up, 3 = left, 4 = down
+var xCoord = GRID_SIZE / 2;
+var yCoord = GRID_SIZE / 2;
+
 $(document).ready(function() {
   canvas = document.getElementById('myCanvas');
   context = canvas.getContext('2d');
   setUpCanvas();
   setUpBoard();
 
-
+  nIntervId = setInterval(display, REFRESH_RATE);
   document.addEventListener('keydown', function(event) {
-    if (event.keyCode == 37) {
-      //alert('Left was pressed');
-      nIntervId = setInterval(display, REFRESH_RATE);
-    }
-    else if (event.keyCode == 39) {
-      //alert('Right was pressed');
-      stopDisplay()
+    if (event.keyCode == 37) { // Left Arrow
+      direction = 3;
+    } else if (event.keyCode == 38) { // Up Arrow
+      direction = 2;
+    } else if (event.keyCode == 39) { // Right Arrow
+      direction = 1;
+    } else if (event.keyCode == 40) { // Down Arrow
+      direction = 4;
     }
   }, true);
 });
@@ -54,20 +59,33 @@ function setUpBoard() {
 }
 
 function display() {
-  var x = Math.floor((Math.random() * GRID_SIZE));
-  var y = Math.floor((Math.random() * GRID_SIZE));
-  board[x][y] = 1;
+  if (direction === 1) {
+    board[xCoord][yCoord] = 0;
+    xCoord++;
+  } else if (direction === 2) {
+    board[xCoord][yCoord] = 0;
+    yCoord--;
+  } else if (direction === 3) {
+    board[xCoord][yCoord] = 0;
+    xCoord--;
+  } else if (direction === 4) {
+    board[xCoord][yCoord] = 0;
+    yCoord++;
+  }
+  board[xCoord][yCoord] = 1;
 
   for (var i = 0; i < GRID_SIZE; i++) {
     for (var j = 0; j < GRID_SIZE; j++) {
       if (board[i][j] === 1) {
-        context.fillStyle = "#FF0000";
-        var xStart = i * CANVAS_SIZE / GRID_SIZE + 1;
-        var yStart = j * CANVAS_SIZE / GRID_SIZE + 1;
-        var xLength = CANVAS_SIZE / GRID_SIZE - 2;
-        var yLength = CANVAS_SIZE / GRID_SIZE - 2;
-        context.fillRect(xStart, yStart, xLength, yLength);
+        context.fillStyle = "#FF0000"; // red
+      } else {
+        context.fillStyle = "#FFFFFF"; // white
       }
+      var xStart = i * CANVAS_SIZE / GRID_SIZE + 1;
+      var yStart = j * CANVAS_SIZE / GRID_SIZE + 1;
+      var xLength = CANVAS_SIZE / GRID_SIZE - 2;
+      var yLength = CANVAS_SIZE / GRID_SIZE - 2;
+      context.fillRect(xStart, yStart, xLength, yLength);
     }
   }
 }

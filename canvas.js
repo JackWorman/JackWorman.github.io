cbf06ef8c60b8f3fd49851cde0b04430ce290959
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-06-08 20:58:11 -0500

    vertical and horizontal grid

--- canvas.js
+++ canvas.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
   var canvas = document.getElementById('myCanvas');
   var context = canvas.getContext('2d');
-  for (var i = 1; i < 10; i++) {
+  for (var i = 1; i < 100; i++) {
     context.moveTo(0, i * 10);
     context.lineTo(1000, i * 10);
+    context.stroke();
+    context.moveTo(i * 10, 0);
+    context.lineTo(i * 10, 1000);
     context.stroke();
   }
 });

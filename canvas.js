d546f074877d5071a3c261b29c3b88ed8c4eef7f
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-06-09 01:14:20 -0500

    reset and controls now are better

--- canvas.js
+++ canvas.js
@@ -4,12 +4,12 @@ const GRID_SIZE = 50;
 const REFRESH_RATE = 125; // time in milliseconds
 
 // Globals
-var gameLoop;
 var canvas;
 var context;
-
+var gameLoop;
 var snake = [];
 var fruit;
+var score = 0;
 
 document.addEventListener("DOMContentLoaded", function(){
   canvas = document.getElementById('myCanvas');
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function(){
   setUpControls();
   snake.push(new Vector(GRID_SIZE / 2, GRID_SIZE / 2, 'none'));
   placeFruit();
-  gameLoop = setInterval(display, REFRESH_RATE);
+  gameLoop = setInterval(refresh, REFRESH_RATE);
 });
 
 function setUpCanvas() {
@@ -48,15 +48,35 @@ function setUpBoard() {
 }
 
 function setUpControls() {
+  /**
+   * 37 = Left Arrow
+   * 38 = Up Arrow
+   * 39 = Right Arrow
+   * 40 = Down Arrow
+  */
   document.addEventListener('keydown', function(event) {
-    if (event.keyCode == 37) { // Left Arrow
-      snake[0].direction = 'left';
-    } else if (event.keyCode == 38) { // Up Arrow
-      snake[0].direction = 'up';
-    } else if (event.keyCode == 39) { // Right Arrow
-      snake[0].direction = 'right';
-    } else if (event.keyCode == 40) { // Down Arrow
-      snake[0].direction = 'down';
+    if (snake[0].direction === 'left' || snake[0].direction === 'right') {
+      if (event.keyCode == 38) {
+        snake[0].direction = 'up';
+      } else if (event.keyCode == 40) {
+        snake[0].direction = 'dowm';
+      }
+    } else if (snake[0].direction === 'up' || snake[0].direction === 'down') {
+      if (event.keyCode == 37) {
+        snake[0].direction = 'left';
+      } else if (event.keyCode == 39) {
+        snake[0].direction = 'right';
+      }
+    } else {
+      if (event.keyCode == 37) { // Left Arrow
+        snake[0].direction = 'left';
+      } else if (event.keyCode == 38) { // Up Arrow
+        snake[0].direction = 'up';
+      } else if (event.keyCode == 39) { // Right Arrow
+        snake[0].direction = 'right';
+      } else if (event.keyCode == 40) { // Down Arrow
+        snake[0].direction = 'down';
+      }
     }
   }, true);
 }
@@ -77,18 +97,18 @@ function placeFruit() {
   fruit = new Point(fruitX, fruitY);
 }
 
-function display() {
+function refresh() {
   moveSnake();
   // Refresh display.
   for (var i = 0; i < GRID_SIZE; i++) {
     for (var j = 0; j < GRID_SIZE; j++) {
-      context.fillStyle = "#FFFFFF"; // white
+      context.fillStyle = "#FFFFFF"; // White
       if (i === fruit.x && j === fruit.y) {
-        context.fillStyle = "#00FF00"; // ???
+        context.fillStyle = "#00FF00"; // Green
       }
       for (var k = 0; k < snake.length; k++) {
         if (i === snake[k].x && j === snake[k].y) {
-          context.fillStyle = "#FF0000"; // red
+          context.fillStyle = "#FF0000"; // Red
           break;
         }
       }
@@ -102,6 +122,7 @@ function display() {
 }
 
 function moveSnake() {
+  // Move the snake from tail to head.
   for (var i = snake.length - 1; i >= 0; i--) {
     if (snake[i].direction === 'right') {
       snake[i].x++;
@@ -127,6 +148,7 @@ function moveSnake() {
   if (hitSelf || snake[0].x < 0 || snake[0].y < 0 || snake[0].x >= GRID_SIZE || snake[0].y >= GRID_SIZE) {
     clearInterval(gameLoop);
     alert('GAME OVER!');
+    reset();
     return;
   }
   // Test if the snake ate a fruit.
@@ -136,6 +158,12 @@ function moveSnake() {
       snake.push(new Vector(snake[snake.length - 1].x, snake[snake.length - 1].y, 'none'));
     }
   }
+}
+
+function reset() {
+  snake = [new Vector(GRID_SIZE / 2, GRID_SIZE / 2, 'none')];
+  placeFruit();
+  score = 0;
 }
 
 class Vector {

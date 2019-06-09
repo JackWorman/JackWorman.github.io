c9e96e3bddccac388cf73c94f83dbcd0627d299d
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-06-08 21:11:21 -0500

    increase size but keep same ratio of grid

--- canvas.js
+++ canvas.js
@@ -1,12 +1,16 @@
+var CANVAS_SIZE = 750;
+
 $(document).ready(function() {
   var canvas = document.getElementById('myCanvas');
+  canvas.style.width = CANVAS_SIZE;
+  canvas.style.height = CANVAS_SIZE;
   var context = canvas.getContext('2d');
   for (var i = 1; i < 50; i++) {
-    context.moveTo(0, i * 10);
-    context.lineTo(500, i * 10);
+    context.moveTo(0, i * CANVAS_SIZE / 50);
+    context.lineTo(CANVAS_SIZE, i * CANVAS_SIZE / 50);
     context.stroke();
-    context.moveTo(i * 10, 0);
-    context.lineTo(i * 10, 500);
+    context.moveTo(i * CANVAS_SIZE / 50, 0);
+    context.lineTo(i * CANVAS_SIZE / 50, CANVAS_SIZE);
     context.stroke();
   }
 });

d715569b3e5008aacca78ab49b722237bd6b9fa4
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-08-12 11:32:03 -0500

    colors

--- Checkers/scripts/Board.js
+++ Checkers/scripts/Board.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const CHECKER_COLOR_1 = 'rgb(139, 69, 19)';
+const CHECKER_COLOR_2 = 'rgb(222, 184, 135)';
+const HIGHLIGHT_COLOR = 'rgb(153, 204, 255)';
+const SELECT_COLOR = 'rgb(0, 0, 255)';
+
 export default class Board {
   constructor() {
     this.pieces = [];
@@ -16,27 +21,27 @@ export default class Board {
   }
 
   render(context, highlightedCoordinates, selectedCoordinates) {
-    CONTEXT_BACKGROUND.fillStyle = CHECKER_COLOR_2;
-    CONTEXT_BACKGROUND.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
-    CONTEXT_BACKGROUND.fillStyle = CHECKER_COLOR_1;
+    context.fillStyle = CHECKER_COLOR_1;
+    context.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    context.fillStyle = CHECKER_COLOR_2;
     // Draws the checker board
     for (var col = 0; col < GRID_SIZE; col++) {
       for (var row = 0; row < GRID_SIZE; row++) {
         if ((col % 2 === 0 && row % 2 === 0) || (col % 2 !== 0 && row % 2 !== 0)) {
-          CONTEXT_BACKGROUND.fillRect(col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
+          context.fillRect(col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
         }
       }
     }
     // Draws highlighted square
     if (mouseCoordinates !== null) {
-      CONTEXT_BACKGROUND.fillStyle = HIGHLIGHT_COLOR;
-      CONTEXT_BACKGROUND.fillRect(mouseCoordinates.col * SQUARE_SIZE, mouseCoordinates.row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
+      context.fillStyle = HIGHLIGHT_COLOR;
+      context.fillRect(mouseCoordinates.col * SQUARE_SIZE, mouseCoordinates.row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
     }
     // Draws selected square
     if (selectedCoordinates !== null) {
-      CONTEXT_BACKGROUND.fillStyle = SELECT_COLOR;
-      CONTEXT_BACKGROUND.fillRect(selectedCoordinates.col * SQUARE_SIZE, selectedCoordinates.row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
-      highlightAvailableMoves();
+      context.fillStyle = SELECT_COLOR;
+      context.fillRect(selectedCoordinates.col * SQUARE_SIZE, selectedCoordinates.row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
+      // highlightAvailableMoves();
     }
   }
 }

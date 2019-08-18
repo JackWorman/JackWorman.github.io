aede59e19731056766d955377dc89547ab0156b3
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-08-18 18:09:32 -0500

    fd

--- Checkers/scripts/Piece.js
+++ Checkers/scripts/Piece.js
@@ -52,24 +52,19 @@ export default class Piece {
   }
 
   calculateJumps(pieces, col, row, jumps) {
-    const func2 = function(pieces, col, row, moves, jumps) {
-      console.log('=================');
-      console.log(is2DArrayDefined(pieces, col - 1, row - 1));
-      console.log(pieces[col][row].player === PLAYER_2);
-      console.log(pieces[col + 1][row - 1] === 'empty');
-      if (is2DArrayDefined(pieces, col - 1, row - 1) && pieces[col][row].player === PLAYER_2 && pieces[col - 1][row - 1] === 'empty') {
-        console.log('tests');
+    const func2 = function(pieces, col, row, dCol, dRow, moves, jumps) {
+      if (is2DArrayDefined(pieces, col + dCol * 2, row + dRow * 2) && pieces[col + dCol][row + dRow].player === PLAYER_2 && pieces[col + dCol * 2][row + dRow * 2] === 'empty') {
         let newJumps = jumps.slice(0);
         newJumps.push({col: col, row: row});
-        moves.push({col: col - 1, row: row - 1, jumps: newJumps});
-        moves = moves.concat(this.calculateJumps(pieces, col - 1, row - 1, newJumps));
+        moves.push({col: col + dCol * 2, row: row + dRow * 2, jumps: newJumps});
+        moves = moves.concat(this.calculateJumps(pieces, col + dCol * 2, row + dRow * 2, newJumps));
       }
       return moves;
     }
     let moves = [];
     if (this.player === PLAYER_1) {
-      moves = func2(pieces, col - 1, row - 1, moves, jumps);
-      moves = func2(pieces, col + 1, row - 1, moves, jumps);
+      moves = func2(pieces, col, row, -1, -1, moves, jumps);
+      moves = func2(pieces, col, row, 1, -1, moves, jumps);
       // if (is2DArrayDefined(pieces, col - 2, row - 2) && pieces[col - 1][row - 1].player === PLAYER_2 && pieces[col - 2][row - 2] === 'empty') {
       //   let newJumps = jumps.slice(0);
       //   newJumps.push({col: col - 1, row: row - 1});

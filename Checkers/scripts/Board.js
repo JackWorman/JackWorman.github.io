ae2ef10fe5ef1f478ffb5fb8ac154ae98ded2c31
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-08-18 08:34:32 -0500

    gfgf

--- Checkers/scripts/Board.js
+++ Checkers/scripts/Board.js
@@ -40,6 +40,7 @@ export default class Board {
       CONTEXT_BOARD.fillStyle = SELECT_COLOR;
       CONTEXT_BOARD.fillRect(selectedCoordinates.col * squareSize, selectedCoordinates.row * squareSize, squareSize, squareSize);
     }
+    console.log(moveCoordinates.length);
     for (var i = 0; i < moveCoordinates.length; i++) {
       CONTEXT_BOARD.fillStyle = MOVE_COLOR;
       CONTEXT_BOARD.fillRect(moveCoordinates[i].col * squareSize, moveCoordinates[i].row * squareSize, squareSize, squareSize);

a7213bc60ce252a3672909894efdf49f8d9e1b5e
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-08-20 14:28:50 -0500

    fd

--- Checkers/scripts/main.js
+++ Checkers/scripts/main.js
@@ -37,6 +37,7 @@ CANVAS_CONTAINER.addEventListener('click', function() {
         // Moves piece to the moveCoordinates.
         pieces[selectedCoordinate.col][selectedCoordinate.row].col = moveCoordinate.col;
         pieces[selectedCoordinate.col][selectedCoordinate.row].row = moveCoordinate.row;
+        // Checks if the piece becomes a king.
         if (selectedCoordinate.row === 0 && pieces[selectedCoordinate.col][selectedCoordinate.row].player === 'player-1'
           || selectedCoordinate.row === 7 && pieces[selectedCoordinate.col][selectedCoordinate.row].player === 'player-2') {
           pieces[selectedCoordinate.col][selectedCoordinate.row].isKing = true;

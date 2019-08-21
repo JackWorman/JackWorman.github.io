dd5b6ddccbf42b3e039286505c48530f16e641a2
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-08-21 10:49:48 -0500

    fds

--- Checkers/scripts/Piece.js
+++ Checkers/scripts/Piece.js
@@ -91,10 +91,13 @@ export default class Piece {
     } else if (this.player === PLAYER_2) {
       CONTEXT_PIECES.fillStyle = PLAYER_2_COLOR;
     }
+    CONTEXT_PIECES.fill();
     CONTEXT_PIECES.lineWidth = 5;
     CONTEXT_PIECES.strokeStyle = OUTLINE_COLOR;
     CONTEXT_PIECES.stroke();
-    CONTEXT_PIECES.fill();
+    if (this.isKing) {
+
+    }
   }
 }
 

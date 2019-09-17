21993fea6f7739764754c1831d00b6e99db47f29
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-09-17 09:02:50 -0500

    cleaned up snake.js

--- Snake/scripts/snake.js
+++ Snake/scripts/snake.js
@@ -14,23 +14,14 @@ export class Snake {
 
   move() {
     this.body.pop();
-    // // Move the snake from tail to head.
-    // for (let i = this.body.length - 1; i > 0; i--) {
-    //   this.body[i] = {x: this.body[i - 1].x, y: this.body[i - 1].y};
-    // }
-    // Move the head of the snake.
     if (this.direction === 'left') {
       this.body.unshift({x: this.body[0].x - 1, y: this.body[0].y});
-      // this.body[0] = {x: this.body[0].x - 1, y: this.body[0].y};
     } else if (this.direction === 'up') {
       this.body.unshift({x: this.body[0].x, y: this.body[0].y - 1});
-      // this.body[0] = {x: this.body[0].x, y: this.body[0].y - 1};
     } else if (this.direction === 'right') {
       this.body.unshift({x: this.body[0].x + 1, y: this.body[0].y});
-      // this.body[0] = {x: this.body[0].x + 1, y: this.body[0].y};
     } else if (this.direction === 'down') {
       this.body.unshift({x: this.body[0].x, y: this.body[0].y + 1});
-      // this.body[0] = {x: this.body[0].x, y: this.body[0].y + 1};
     }
   }
 
@@ -55,18 +46,18 @@ export class Snake {
     return this.body[0].x === fruit.x && this.body[0].y === fruit.y;
   }
 
-  render(context) {
-    context.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
-    // Render snake from head to tail.
-    for (let i = snake.body.length - 1; i >= 0; i--) {
-      context.fillStyle = RAINBOW[i % RAINBOW.length];
-      let squareLength = CANVAS_SIZE / GRID_SIZE;
-      context.fillRect(
-        this.body[i].x * squareLength + 0.5,
-        this.body[i].y * squareLength + 0.5,
-        squareLength,
-        squareLength
-      );
-    }
+  // render(context) {
+  //   context.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  //   // Render snake from head to tail.
+  //   for (let i = snake.body.length - 1; i >= 0; i--) {
+  //     context.fillStyle = RAINBOW[i % RAINBOW.length];
+  //     let squareLength = CANVAS_SIZE / GRID_SIZE;
+  //     context.fillRect(
+  //       this.body[i].x * squareLength + 0.5,
+  //       this.body[i].y * squareLength + 0.5,
+  //       squareLength,
+  //       squareLength
+  //     );
+  //   }
   }
 }

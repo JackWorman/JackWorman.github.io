735d207440b9c9a7193bb730dab6c8e576663474
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-09-17 10:42:57 -0500

    using typescript

---
+++ Snake/scripts/KeyCode2.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var KeyCode = /** @class */ (function () {
+    function KeyCode() {
+        this.leftArrow = 37;
+        this.upArrow = 38;
+        this.rightArrow = 39;
+        this.downArrow = 40;
+        this.a = 65;
+        this.d = 68;
+        this.s = 83;
+        this.w = 87;
+    }
+    return KeyCode;
+}());
+exports.KeyCode = KeyCode;
---
+++ Snake/scripts/KeyCode2.ts
@@ -0,0 +1,10 @@
+export class KeyCode {
+  leftArrow = 37;
+  upArrow = 38;
+  rightArrow = 39;
+  downArrow = 40;
+  a = 65;
+  d = 68;
+  s = 83;
+  w = 87;
+}

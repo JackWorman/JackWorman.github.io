734193d3476368cb8a3dda4757bb655305246d91
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-09-18 20:51:38 -0500

    asdf

--- Snake/scripts/main.js
+++ Snake/scripts/main.js
@@ -96,7 +96,7 @@ function scaleDOM() {
   // SPAN_TITLE.style.fontSize = (100 * scaleFactor) + 'px';
   // SPAN_SCORE.style.fontSize = SPAN_HIGHSCORE.style.fontSize = (48 * scaleFactor) + 'px';
   // DIV_HEADER_CONTAINER.style.width = (canvasSize + 2) + 'px';
-  // CANVAS_FOREGROUND.width = CANVAS_FOREGROUND.height = canvasSize;
+  CANVAS_FOREGROUND.width = CANVAS_FOREGROUND.height = canvasSize;
   renderForeground();
 }
 

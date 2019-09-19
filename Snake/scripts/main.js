5ba9eb258bbdadf024dfc1cd4209ef435f8c59ae
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-09-18 20:27:22 -0500

    asdf

--- Snake/scripts/main.js
+++ Snake/scripts/main.js
@@ -93,7 +93,7 @@ function scaleDOM() {
   let scaleFactor = Math.min(scaleFactorW, scaleFactorH);
   canvasSize = 690 * scaleFactor;
   // SPAN_FPS.style.fontSize = (16 * scaleFactor) + 'px';
-  SPAN_TITLE.style.fontSize = (100 * scaleFactor) + 'px';
+  // SPAN_TITLE.style.fontSize = (100 * scaleFactor) + 'px';
   SPAN_SCORE.style.fontSize = SPAN_HIGHSCORE.style.fontSize = (48 * scaleFactor) + 'px';
   DIV_HEADER_CONTAINER.style.width = (canvasSize + 2) + 'px';
   CANVAS_FOREGROUND.width = CANVAS_FOREGROUND.height = canvasSize;

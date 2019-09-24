c81fc0d902af45741b423dd40df998293cbdb696
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-09-23 20:01:43 -0500

    comments

--- Snake/scripts/Pellet.js
+++ Snake/scripts/Pellet.js
@@ -8,6 +8,12 @@ export class Pellet extends Coordinate {
     Object.seal(this);
   }
 
+  /**
+   * Randomly sets the coordinates of the pellet and then checks if it is in a free space. The coordinates are randomly
+   * set till it is in a valid position.
+   * @param  {Number} gridSize [description]
+   * @param  {Snake} snake    [description]
+   */
   placePellet(gridSize, snake) {
     let collison;
     do {

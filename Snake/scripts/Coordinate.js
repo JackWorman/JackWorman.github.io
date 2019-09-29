86eeac21ed070082e831aefe1ab81752acc1ea3e
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-09-29 08:20:53 -0500

    asdf

--- Snake/scripts/Coordinate.js
+++ Snake/scripts/Coordinate.js
@@ -2,8 +2,8 @@
 
 export class Coordinate {
   constructor(x, y) {
-    Object.defineProperty(this, 'x', {value: -1, writable: true});
-    Object.defineProperty(this, 'y', {value: -1, writable: true});
+    Object.defineProperty(this, 'x', {value: x, writable: true});
+    Object.defineProperty(this, 'y', {value: y, writable: true});
     Object.seal(this);
   }
 

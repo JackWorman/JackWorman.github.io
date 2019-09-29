d3470ab03480906e7108916258ca93414e742566
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-09-29 08:27:39 -0500

    asdf

--- Snake/scripts/Snake.js
+++ Snake/scripts/Snake.js
@@ -10,10 +10,9 @@ export class Snake {
   constructor(startX, startY) {
     Object.defineProperty(this, 'direction', {value: 'none', writable: true});
     Object.defineProperty(this, 'bodySegments', {value: []});
-    for (let i = 0; i < STARTING_SIZE; i++) {
-      // this.bodySegments.push({x: startX, y: startY});
-      this.bodySegments.push(new Coordinate(startX, startY));
-    }
+    // for (let i = 0; i < STARTING_SIZE; i++) {
+    //   this.bodySegments.push(new Coordinate(startX, startY));
+    // }
     Object.seal(this);
   }
 
@@ -24,25 +23,18 @@ export class Snake {
   move() {
     this.bodySegments.pop();
     if (this.direction === 'left') {
-      // this.bodySegments.unshift({x: this.bodySegments[0].x - 1, y: this.bodySegments[0].y});
       this.bodySegments.unshift(new Coordinate(this.bodySegments[0].x - 1, this.bodySegments[0].y));
     } else if (this.direction === 'up') {
-      // this.bodySegments.unshift({x: this.bodySegments[0].x, y: this.bodySegments[0].y - 1});
       this.bodySegments.unshift(new Coordinate(this.bodySegments[0].x, this.bodySegments[0].y - 1));
     } else if (this.direction === 'right') {
-      // this.bodySegments.unshift({x: this.bodySegments[0].x + 1, y: this.bodySegments[0].y});
       this.bodySegments.unshift(new Coordinate(this.bodySegments[0].x + 1, this.bodySegments[0].y));
     } else if (this.direction === 'down') {
-      // this.bodySegments.unshift({x: this.bodySegments[0].x, y: this.bodySegments[0].y + 1});
       this.bodySegments.unshift(new Coordinate(this.bodySegments[0].x, this.bodySegments[0].y + 1));
     }
   }
 
   grow() {
     for (let i = 0; i < GROW_RATE; i++) {
-      // this.bodySegments.push(
-      //   {x: this.bodySegments[this.bodySegments.length - 1].x, y: this.bodySegments[this.bodySegments.length - 1].y}
-      // );
       this.bodySegments.push(new Coordinate(
         this.bodySegments[this.bodySegments.length - 1].x, this.bodySegments[this.bodySegments.length - 1].y
       ));
@@ -52,7 +44,7 @@ export class Snake {
   checkCollison(gridSize) {
     // Checks if the snake hit itself.
     for (let i = 1; i < this.bodySegments.length; i++) {
-      if (this.bodySegments[0].x === this.bodySegments[i].x && this.bodySegments[0].y === this.bodySegments[i].y) {
+      if (Coordinate.compare(this.bodySegments[0], this.bodySegments[i])) {
         return true;
       }
     }
@@ -64,16 +56,16 @@ export class Snake {
   }
 
   checkFruitEaten(pellet) {
-    return this.bodySegments[0].x === pellet.x && this.bodySegments[0].y === pellet.y;
+    return Coordinate.compare(this.bodySegments[0], pellet);
   }
 
-  reset(startX, startY) {
+  reset(x, y) {
     this.direction = 'none';
     while (this.bodySegments.length > 0) {
       this.bodySegments.pop();
     }
     for (let i = 0; i < STARTING_SIZE; i++) {
-      this.bodySegments.push(new Coordinate(startX, startY));
+      this.bodySegments.push(new Coordinate(x, y));
     }
   }
 }

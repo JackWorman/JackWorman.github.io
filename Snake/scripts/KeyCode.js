3c37cb81ad80f814ae4a0f5ee6a6d63fc40fded7
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-09-28 19:56:27 -0500

    asdf

--- Snake/scripts/KeyCode.js
+++ Snake/scripts/KeyCode.js
@@ -1,16 +1,5 @@
 'use strict';
 
-// export class KeyCode {}
-//
-// KeyCode.leftArrow = 37;
-// KeyCode.upArrow = 38;
-// KeyCode.rightArrow = 39;
-// KeyCode.downArrow = 40;
-// KeyCode.a = 65;
-// KeyCode.d = 68;
-// KeyCode.s = 83;
-// KeyCode.w = 87;
-
 export const leftArrow = 37;
 export const upArrow = 38;
 export const rightArrow = 39;

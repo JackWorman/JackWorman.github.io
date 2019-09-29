92f1b066746b247d2d357ad9bc229f71526cc239
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-09-28 22:36:38 -0500

    deltaTimes

--- Snake/scripts/FrameRate.js
+++ Snake/scripts/FrameRate.js
@@ -4,24 +4,24 @@ const MILLISECONDS_PER_SECOND = 1000;
 const SPAN_FPS = document.getElementById('span-fps');
 
 let initialCall = true;
-let deltas = [];
+let deltaTimes = [];
 let then;
 
 export function calculate() {
    // First time setup.
   if (initialCall) {
-    deltas = [];
+    deltaTimes = [];
     then = performance.now();
     initialCall = false;
     return;
   }
   let now = performance.now();
-  deltas.push(now - then);
-  if (deltas.length > 100) {
-    deltas.shift();
+  deltaTimes.push(now - then);
+  if (deltaTimes.length > 100) {
+    deltaTimes.shift();
   }
-  let averageDelta = (deltas.reduce((a, b) => (a + b)) / deltas.length);
-  SPAN_FPS.textContent = 'FPS: ' + (MILLISECONDS_PER_SECOND / averageDelta).toFixed(2);
+  let averageDeltaTime = (deltaTimes.reduce((a, b) => (a + b)) / deltaTimes.length);
+  SPAN_FPS.textContent = 'FPS: ' + (MILLISECONDS_PER_SECOND / averageDeltaTime).toFixed(2);
   then = now;
 }
 

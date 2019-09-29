22a4e70389512f37404add71fbf0ee272cda3cd9
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-09-29 08:07:25 -0500

    adsf

--- Snake/scripts/Snake.js
+++ Snake/scripts/Snake.js
@@ -25,6 +25,7 @@ export class Snake {
     console.log(this.bodySegments);
     this.bodySegments.pop();
     console.log(this.bodySegments);
+    console.log(this.direction);
     if (this.direction === 'left') {
       // this.bodySegments.unshift({x: this.bodySegments[0].x - 1, y: this.bodySegments[0].y});
       this.bodySegments.unshift(new Coordinate(this.bodySegments[0].x - 1, this.bodySegments[0].y));

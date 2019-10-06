3a9d0ee360a6a006fcd7f45e28984965bad5df1a
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-10-06 13:09:44 -0500

    asdf

--- Test/scripts/main.js
+++ Test/scripts/main.js
@@ -16,12 +16,13 @@ const words = loadFile('https://jackworman.com/Test/scripts/words.txt').split(/\
 const counts = [];
 
 for (const word of words) {
-  for (let i = 0; word.length - 1; i++) {
+  for (let i = 0; i < word.length - 1; i++) {
     if (typeof counts[word.substr(i, i + 1)] === 'undefined') {
       counts[word.substr(i, i + 1)] = 1;
     } else {
       counts[word.substr(i, i + 1)]++;
     }
   }
-  console.log(counts);
 }
+
+console.log(counts);

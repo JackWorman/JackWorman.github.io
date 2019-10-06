4cc68171d39489c329027a99dc3d7d099b1ce8d6
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-10-06 12:13:31 -0500

    https://jackworman.com/Test/

--- Test/scripts/main.js
+++ Test/scripts/main.js
@@ -5,7 +5,7 @@ console.log(loadFile('words.txt'));
 function loadFile(filePath) {
   var result = null;
   var xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET", filePath, false);
+  xmlhttp.open("GET", 'https://jackworman.com/Test/scripts/words.txt', false);
   xmlhttp.send();
   if (xmlhttp.status==200) {
     result = xmlhttp.responseText;

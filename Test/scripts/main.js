6f4a27dc260ed0283614bc4f4692f3d470037934
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-10-06 12:08:42 -0500

    test

---
+++ Test/scripts/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+console.log(loadFile('words.txt'));
+
+function loadFile(filePath) {
+  var result = null;
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", filePath, false);
+  xmlhttp.send();
+  if (xmlhttp.status==200) {
+    result = xmlhttp.responseText;
+  }
+  return result;
+}

548fb6c957fd26874954653452fcfb22c075bd0d
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-10-07 07:35:24 -0500

    asdf

--- Test/scripts/main.js
+++ Test/scripts/main.js
@@ -3,7 +3,7 @@
 function loadFile(filePath) {
   let result = null;
   const xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET", filePath, false);
+  xmlhttp.open('GET', filePath, false);
   xmlhttp.send();
   if (xmlhttp.status === 200) {
     result = xmlhttp.responseText;

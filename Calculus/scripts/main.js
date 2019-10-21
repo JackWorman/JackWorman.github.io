30ef5168cefb92711baa95fbf5d3216ad52165b5
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-10-20 20:12:45 -0500

    initial setup for calculus

---
+++ Calculus/scripts/main.js
@@ -0,0 +1,7 @@
+// DOM elements
+const CANVAS_1 = document.getElementById('canvas-1');
+const CANVAS_2 = document.getElementById('canvas-2');
+const CANVAS_3 = document.getElementById('canvas-3');
+const CONTEXT_1 = CANVAS_1.getContext('2d');
+const CONTEXT_2 = CANVAS_2.getContext('2d');
+const CONTEXT_3 = CANVAS_3.getContext('2d');

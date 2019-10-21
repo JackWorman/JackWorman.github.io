19297185086bb3d017e1b65878f2b70cb9e5d3cd
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-10-21 08:34:16 -0500

    asdf

--- Calculus/scripts/main.js
+++ Calculus/scripts/main.js
@@ -8,41 +8,39 @@ const CONTEXT_3 = CANVAS_3.getContext('2d');
 
 let isDrawing = false;
 let boundingClientRect = CANVAS_1.getBoundingClientRect();
+let canvasCoordinates = {x: -1, y: -1};
 let x = 0;
 let y = 0;
 
 
-CONTEXT_1.strokeStyle = 'white';
-CONTEXT_1.lineWidth = 3;
+
 
 
 CANVAS_1.addEventListener('mousedown', e => {
   isDrawing = true;
-  x = e.clientX - boundingClientRect.left;
-  y = e.clientY - boundingClientRect.top;
+  CONTEXT_1.strokeStyle = 'white';
+  CONTEXT_1.lineWidth = 3;
   CONTEXT_1.beginPath();
-  CONTEXT_1.moveTo(x, y);
+  CONTEXT_1.moveTo(canvasCoordinates.x, canvasCoordinates.y);
 });
 
 CANVAS_1.addEventListener('mousemove', e => {
   if (isDrawing) {
-    x = e.clientX - boundingClientRect.left;
-    y = e.clientY - boundingClientRect.top;
-    CONTEXT_1.lineTo(x, y);
+    canvasCoordinates = {x: e.clientX - boundingClientRect.left, y: e.clientY - boundingClientRect.top};
+    CONTEXT_1.lineTo(canvasCoordinates.x, canvasCoordinates.y);
     CONTEXT_1.stroke();
     CONTEXT_1.closePath();
     //
     CONTEXT_1.beginPath();
-    CONTEXT_1.moveTo(x, y);
+    CONTEXT_1.moveTo(canvasCoordinates.x, canvasCoordinates.y);
   }
 });
 
 CANVAS_1.addEventListener('mouseup', e => {
   if (isDrawing) {
     isDrawing = true;
-    x = e.clientX - boundingClientRect.left;
-    y = e.clientY - boundingClientRect.top;
-    CONTEXT_1.lineTo(x, y);
+    canvasCoordinates = {x: e.clientX - boundingClientRect.left, y: e.clientY - boundingClientRect.top};
+    CONTEXT_1.lineTo(canvasCoordinates.x, canvasCoordinates.y);
     CONTEXT_1.stroke();
     CONTEXT_1.closePath();
   }

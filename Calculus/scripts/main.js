a3f8e9ec69c3338980032da6af92eae67de2b32b
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-10-21 08:15:58 -0500

    drawing

--- Calculus/scripts/main.js
+++ Calculus/scripts/main.js
@@ -6,14 +6,44 @@ const CONTEXT_1 = CANVAS_1.getContext('2d');
 const CONTEXT_2 = CANVAS_2.getContext('2d');
 const CONTEXT_3 = CANVAS_3.getContext('2d');
 
+let isDrawing = false;
+let boundingClientRect = CANVAS_1.getBoundingClientRect();
+let x = 0;
+let y = 0;
+
+
+CANVAS_1.strokeStyle = 'white';
+CANVAS_1.lineWidth = 3;
+
+
+CANVAS_1.addEventListener('mousedown', () => {
+  isDrawing = true;
+  x = e.clientX - boundingClientRect.left;
+  y = e.clientY - boundingClientRect.top;
+  CONTEXT_1.beginPath();
+  CONTEXT_1.moveTo(x, y);
+});
+
 CANVAS_1.addEventListener('mousemove', () => {
-  alert('mousemove');
+  if (isDrawing) {
+    x = e.clientX - boundingClientRect.left;
+    y = e.clientY - boundingClientRect.top;
+    CONTEXT_1.lineTo(x, y);
+    CONTEXT_1.stroke();
+    CONTEXT_1.closePath();
+    //
+    CONTEXT_1.beginPath();
+    CONTEXT_1.moveTo(x, y);
+  }
 });
 
-CANVAS_2.addEventListener('mousedown', () => {
-  alert('mousedown');
-});
-
-CANVAS_3.addEventListener('mouseup', () => {
-  alert('mouseup');
+CANVAS_1.addEventListener('mouseup', () => {
+  if (isDrawing) {
+    isDrawing = true;
+    x = e.clientX - boundingClientRect.left;
+    y = e.clientY - boundingClientRect.top;
+    CONTEXT_1.lineTo(x, y);
+    CONTEXT_1.stroke();
+    CONTEXT_1.closePath();
+  }
 });

b5b0f22427f75d42b914163df1c86bd755694983
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-11-03 12:20:09 -0600

    debugging v3

--- Asteroids/scripts/ScaleCanvas.js
+++ Asteroids/scripts/ScaleCanvas.js
@@ -11,13 +11,16 @@ export let canvasSize;
 /**
  * This must be done in Javascript because it clears the canvas constantly when using calc() in CSS.
  */
-function scaleCanvas() {
+export function scaleCanvas() {
   canvasSize = BASE_CANVAS_SIZE * Math.min(document.body.clientWidth, document.body.clientHeight) / BASE_SIZE;
   CANVAS_FOREGROUND.width = CANVAS_FOREGROUND.height = canvasSize;
   window.requestAnimationFrame(render);
-  console.log(canvasSize)
+  console.log(`==========`);
+  console.log(`canvasSize: ${canvasSize}`);
+  console.log(`width: ${document.body.clientWidth}`);
+  console.log(`height: ${document.body.clientHeight}`);
 };
 
-window.onload = scaleCanvas;
+// window.onload = scaleCanvas;
 window.addEventListener(`load`, scaleCanvas);
 window.addEventListener(`resize`, scaleCanvas);

39b1e93522e90abbab2c966cca8546e2a3048bd0
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-11-05 11:02:24 -0600

    syntax clean up for laser.js

--- Asteroids/scripts/laser.js
+++ Asteroids/scripts/laser.js
@@ -1,15 +1,15 @@
-'use strict';
+"use strict";
 
-import Asteroid from './asteroid.js';
+import Asteroid from "./asteroid.js";
 
 const RAINBOW = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 127, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 0, 255)",
-  "rgb(75, 0, 130)",
-  "rgb(148, 0, 211)",
+  `rgb(255, 0, 0)`,
+  `rgb(255, 127, 0)`,
+  `rgb(255, 255, 0)`,
+  `rgb(0, 255, 0)`,
+  `rgb(0, 0, 255)`,
+  `rgb(75, 0, 130)`,
+  `rgb(148, 0, 211)`,
 ];
 const MAXIMUM_ASTEROID_RADIUS = 100;
 const MILLISECONDS_PER_SECOND = 1000;
@@ -25,8 +25,8 @@ export default class Laser {
   }
 
   move(canvasSize, deltaTime) {
-    var dx = this.speed * deltaTime / MILLISECONDS_PER_SECOND * Math.cos(this.angle);
-    var dy = this.speed * deltaTime / MILLISECONDS_PER_SECOND * Math.sin(this.angle);
+    let dx = this.speed * deltaTime / MILLISECONDS_PER_SECOND * Math.cos(this.angle);
+    let dy = this.speed * deltaTime / MILLISECONDS_PER_SECOND * Math.sin(this.angle);
     this.x += dx;
     this.y += dy;
     if (this.x < -MAXIMUM_ASTEROID_RADIUS) this.x += canvasSize + MAXIMUM_ASTEROID_RADIUS * 2;
@@ -38,8 +38,8 @@ export default class Laser {
   }
 
   render(context) {
-    var length = 40;
-    var width = 2;
+    let length = 40;
+    let width = 2;
     context.save();
     context.translate(this.x, this.y);
     context.rotate(this.angle);
@@ -49,30 +49,14 @@ export default class Laser {
     context.lineTo(length, width);
     context.lineTo(-length, width);
     context.closePath();
-    context.fillStyle =  RAINBOW[this.color++ % RAINBOW.length];;
+    context.fillStyle = RAINBOW[this.color++ % RAINBOW.length];;
     context.fill();
     context.restore();
   }
 
-  // render(context) {
-  //   context.save();
-  //   context.translate(this.x, this.y);
-  //   context.rotate(this.angle);
-  //   context.beginPath();
-  //   context.moveTo(this.x, this.y);
-  //   context.quadraticCurveTo(this.x, this.y + 2, this.x + 20, this.y + 2);
-  //   context.quadraticCurveTo(this.x + 40, this.y + 2, this.x + 40, this.y);
-  //   context.quadraticCurveTo(this.x + 40, this.y - 2, this.x + 20, this.y - 2);
-  //   context.quadraticCurveTo(this.x, this.y - 2, this.x, this.y);
-  //   context.closePath();
-  //   context.fillStyle = 'rgb(0, 255, 0)';
-  //   context.fill();
-  //   context.restore();
-  // }
-
   detectCollison(asteroids) {
-    for (var i = 0; i < asteroids.length; i++) {
-      var distance = Math.sqrt(Math.pow(this.x - asteroids[i].x, 2) + Math.pow(this.y - asteroids[i].y, 2));
+    for (let i = 0; i < asteroids.length; i++) {
+      let distance = Math.sqrt(Math.pow(this.x - asteroids[i].x, 2) + Math.pow(this.y - asteroids[i].y, 2));
       if (distance < asteroids[i].radius) {
         if (asteroids[i].size !== 0) {
           asteroids.push(new Asteroid(asteroids[i].x, asteroids[i].y, asteroids[i].size - 1));
@@ -84,18 +68,4 @@ export default class Laser {
     }
     return false;
   }
-}
-
-function drawOval(x, y, rw, rh)
-{
-  var canvas = document.getElementById("canvas4");
-  var context = canvas.getContext("2d");
-  context.save();
-  context.scale(1,  rh/rw);
-  context.beginPath();
-  context.arc(x, y, rw, 0, 2 * Math.PI);
-  context.restore();
-  context.lineWidth=4;
-  context.strokeStyle="orange";
-  context.stroke();
 }

8873f2518ddeff3585173eddc4ffb2593e8678cc
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-11-09 17:02:54 -0600

    testing getting lines from txt file

---
+++ TypingTrainer/scripts/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function loadFile(filePath) {
+  const xmlHttpRequest = new xmlHttpRequestRequest();
+  xmlHttpRequest.open(`GET`, filePath, false);
+  xmlHttpRequest.send();
+  if (xmlHttpRequest.status === 200) {
+    return xmlHttpRequest.responseText;
+  } else {
+    // Error
+  }
+}
+
+const lines = loadFile(`https://jackworman.com/TypingTrainer/NextLetterCounts.txt`).split(/\n/);
+console.log(lines);

38f13f81d6444a329de28544ca988add3788f6d7
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-11-10 10:05:49 -0600

    words.txt

--- TypingTrainer/scripts/main.js
+++ TypingTrainer/scripts/main.js
@@ -11,54 +11,62 @@ function loadFile(filePath) {
   }
 }
 
-const DIV_TEXT = document.getElementById(`div-text`);
-const lines = loadFile(`https://jackworman.com/TypingTrainer/NextLetterCounts.txt`).split(/\n/);
-const letterCounts = {};
-let allLetterCount = 0;
-
-for (const line of lines) {
-  const letter = line.charAt(0);
-  const words = line.split(/\s/);
-  if (typeof letterCounts[letter] === `undefined`) {
-    letterCounts[letter] = Number(words[2]);
-  } else {
-    letterCounts[letter] += Number(words[2]);
-  }
-  allLetterCount += Number(words[2]);
-}
-
+const words = loadFile(`https://jackworman.com/TypingTrainer/words.txt`).split(/\n/);
 let text = ``;
-for (let i = 0; i < 1000; i++) {
-  if (text === `` || text.charAt(text.length - 1) === ` `) {
-    const rand = Math.floor(Math.random() * allLetterCount);
-    let count = 0;
-    for (const letter in letterCounts) {
-      count += letterCounts[letter];
-      if (rand < count) {
-        text += letter;
-        break;
-      }
-    }
-  } else {
-    const previousLetter = text.charAt(text.length - 1);
-    const rand = Math.floor(Math.random() * letterCounts[previousLetter]);
-    let count = 0;
-    for (const line of lines) {
-      if (line.charAt(0) === previousLetter) {
-        const words = line.split(/\s/);
-        count += Number(words[2]);
-        if (rand < count) {
-          if (words[1] === `space`) {
-            text += ` `;
-          } else {
-            text += words[1];
-          }
-          break;
-        }
-      }
-    }
-  }
+for (let i = 0; i < 100; i++) {
+  text += `${words[Math.floor(Math.random() * words.length)]} `;
 }
 DIV_TEXT.textContent = text;
 
-// console.log(letterCounts);
+
+// const DIV_TEXT = document.getElementById(`div-text`);
+// const lines = loadFile(`https://jackworman.com/TypingTrainer/NextLetterCounts.txt`).split(/\n/);
+// const letterCounts = {};
+// let allLetterCount = 0;
+//
+// for (const line of lines) {
+//   const letter = line.charAt(0);
+//   const words = line.split(/\s/);
+//   if (typeof letterCounts[letter] === `undefined`) {
+//     letterCounts[letter] = Number(words[2]);
+//   } else {
+//     letterCounts[letter] += Number(words[2]);
+//   }
+//   allLetterCount += Number(words[2]);
+// }
+//
+// let text = ``;
+// for (let i = 0; i < 1000; i++) {
+//   if (text === `` || text.charAt(text.length - 1) === ` `) {
+//     const rand = Math.floor(Math.random() * allLetterCount);
+//     let count = 0;
+//     for (const letter in letterCounts) {
+//       count += letterCounts[letter];
+//       if (rand < count) {
+//         text += letter;
+//         break;
+//       }
+//     }
+//   } else {
+//     const previousLetter = text.charAt(text.length - 1);
+//     const rand = Math.floor(Math.random() * letterCounts[previousLetter]);
+//     let count = 0;
+//     for (const line of lines) {
+//       if (line.charAt(0) === previousLetter) {
+//         const words = line.split(/\s/);
+//         count += Number(words[2]);
+//         if (rand < count) {
+//           if (words[1] === `space`) {
+//             text += ` `;
+//           } else {
+//             text += words[1];
+//           }
+//           break;
+//         }
+//       }
+//     }
+//   }
+// }
+// DIV_TEXT.textContent = text;
+//
+// // console.log(letterCounts);

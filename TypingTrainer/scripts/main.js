c839bd1fb5d278430b4ae717f52cb32cce9682eb
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-11-12 13:32:41 -0600

    cleaned up if expression

--- TypingTrainer/scripts/main.js
+++ TypingTrainer/scripts/main.js
@@ -76,7 +76,7 @@ function reset() {
 reset();
 
 document.addEventListener(`keypress`, (event) => {
-    if (!((event.keyCode >= 65 && event.keyCode <= 90) || event.keyCode === 8 || event.keyCode === 32)) {
+    if (!(event.keyCode >= 65 && event.keyCode <= 90) && event.keyCode !== 8 && event.keyCode !== 32) {
       return;
     }
     if (event.keyCode === 8) {

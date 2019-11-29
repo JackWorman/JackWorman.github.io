6d80d7b207294e3b504e1109c07682e370725a93
JackWorman <jack.worman@valpo.edu>
2019-11-29 13:11:44 -0600

    orginal fitness eval

--- Snake/Artificial-Intelligence/scripts/EvolutionaryAlgorithm.js
+++ Snake/Artificial-Intelligence/scripts/EvolutionaryAlgorithm.js
@@ -22,10 +22,11 @@ export class EvolutionaryAlgorithm {
   }
 
   evaluateFitness(apples, steps) {
-    this.neuralNetworks[this.specie].fitness += Math.pow(apples, 2);
-      // steps
-      // + (Math.pow(2, apples) + Math.pow(apples, 2.1)*500)
-      // - (Math.pow(apples, 1.2) * Math.pow(0.25 * steps, 1.3));
+    this.neuralNetworks[this.specie].fitness +=
+      // Math.pow(apples, 2);
+      steps
+      + (Math.pow(2, apples) + Math.pow(apples, 2.1)*500)
+      - (Math.pow(apples, 1.2) * Math.pow(0.25 * steps, 1.3));
   }
 
   sort() {

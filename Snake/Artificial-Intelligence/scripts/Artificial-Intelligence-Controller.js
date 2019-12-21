55cbf7fb2b93621dab9e7d768545d61785e2bb13
JackWorman <33815707+JackWorman@users.noreply.github.com>
2019-12-21 17:54:51 -0600

    debugging

--- Snake/Artificial-Intelligence/scripts/Artificial-Intelligence-Controller.js
+++ Snake/Artificial-Intelligence/scripts/Artificial-Intelligence-Controller.js
@@ -130,7 +130,7 @@ function renderNodes(neuralNetwork, evolutionaryAlgorithm) {
       const intensity = neuralNetwork.layers[i].elements[j][0] * 255;
       CONTEXT_NEURAL_NETWORK.beginPath();
       CONTEXT_NEURAL_NETWORK.arc(canvasSize*((i + 1)/(neuralNetwork.layers.length + 1)), canvasSize/(layerSize + 1)*(j + 1), 8, 0, 2*Math.PI);
-      alert(`${canvasSize*((i + 1)/(neuralNetwork.layers.length + 1))} - ${canvasSize/(layerSize + 1)*(j + 1)}`);
+      alert(`layerSize-${layerSize} canvasSize-${canvasSize} j-${j}`);
       CONTEXT_NEURAL_NETWORK.closePath();
       CONTEXT_NEURAL_NETWORK.strokeStyle = `rgb(255, 255, 255)`;
       CONTEXT_NEURAL_NETWORK.lineWidth = 1;

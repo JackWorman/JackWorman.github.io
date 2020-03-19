855b2c54528368b5bfa3b31954d9ed49eb5919df
JackWorman <33815707+JackWorman@users.noreply.github.com>
2020-03-19 10:32:17 -0500

    a

--- TypingTrainer/scripts/main.js
+++ TypingTrainer/scripts/main.js
@@ -164,8 +164,8 @@ function drawKeyboard() {
 
   const CURRENT_CHARACTER = DIV_TEXT.childNodes[indicatorLocation].textContent;
 
-  for (const [rowNumber, rowValue] of KEYBOARD_LAYOUT) {
-    console.log(rowValue);
+  for (const [rowNumber, rowValue] of KEYBOARD_LAYOUT.entries()) {
+    // console.log(rowValue);
   // for (let row = 0; row < KEYBOARD_LAYOUT.length; row++) {
     let xPosition = 0;
     for (const key of rowValue) {

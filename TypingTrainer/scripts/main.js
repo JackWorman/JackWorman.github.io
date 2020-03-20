fea8c831c7792fb77194393346263a20e6f26d32
JackWorman <33815707+JackWorman@users.noreply.github.com>
2020-03-20 10:26:39 -0500

    [{ support added

--- TypingTrainer/scripts/main.js
+++ TypingTrainer/scripts/main.js
@@ -131,7 +131,7 @@ document.addEventListener(`keydown`, (event) => {
   console.log(event);
   // Checks for an invalid key.
   if (!textSetUp || !(event.keyCode >= 65 && event.keyCode <= 90) && event.keyCode !== 8 && event.keyCode !== 32
-  && event.keyCode !== 173 && event.keyCode !== 190 && event.keyCode !== 59 && event.keyCode !== 191) {
+  && event.keyCode !== 173 && event.keyCode !== 190 && event.keyCode !== 59 && event.keyCode !== 191  && event.keyCode !== 219  && event.keyCode !== 221) {
     return;
   }
   event.preventDefault(); // Stops the browser from going to the previous page.

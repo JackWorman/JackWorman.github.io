49890b0ef14c5a8f768c84744665e0c34dc56b2b
JackWorman <jackworman@gmail.com>
2020-09-07 13:11:05 -0500

    asdf

--- GameOfLife/scripts/typescripts/main.ts
+++ GameOfLife/scripts/typescripts/main.ts
@@ -5,14 +5,12 @@ CANVAS_BOARD.width = 800;
 CANVAS_BOARD.height = 800;
 
 
-for (let col: number = 0; col < 100; col++) {
-  for (let row: number = 0; row < 100; row++) {
-    CONTEXT_BOARD.beginPath();
-    CONTEXT_BOARD.moveTo(col * 8, 0);
-    CONTEXT_BOARD.lineTo(col * 8, 800);
-    CONTEXT_BOARD.moveTo(0, row * 8);
-    CONTEXT_BOARD.lineTo(800, row * 8);
-  }
+CONTEXT_BOARD.beginPath();
+for (let col: number = 0; col <= 100; col++) {
+  CONTEXT_BOARD.moveTo(col * 8, 0);
+  CONTEXT_BOARD.lineTo(col * 8, 800);
+  CONTEXT_BOARD.moveTo(0, col * 8);
+  CONTEXT_BOARD.lineTo(800, col * 8);
 }
 CONTEXT_BOARD.closePath();
 CONTEXT_BOARD.stroke();

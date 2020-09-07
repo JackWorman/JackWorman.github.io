55c8cd985d701ba639e2791c3279b38f87bdf1ae
JackWorman <jackworman@gmail.com>
2020-09-07 17:54:45 -0500

    es3

--- GameOfLife/scripts/javascripts/Helper.js
+++ GameOfLife/scripts/javascripts/Helper.js
@@ -1,14 +1,19 @@
-export function getHTMLCanvasElementById(id) {
-    const htmlCanvasElement = document.getElementById(id);
+"use strict";
+exports.__esModule = true;
+exports.getCanvasRenderingContext2D = exports.getHTMLCanvasElementById = void 0;
+function getHTMLCanvasElementById(id) {
+    var htmlCanvasElement = document.getElementById(id);
     if (htmlCanvasElement instanceof HTMLCanvasElement) {
         return htmlCanvasElement;
     }
     throw new Error('Could not get HTMLCanvasElement.');
 }
-export function getCanvasRenderingContext2D(htmlCanvasElement) {
-    const canvasRenderingContext2D = htmlCanvasElement.getContext('2d');
+exports.getHTMLCanvasElementById = getHTMLCanvasElementById;
+function getCanvasRenderingContext2D(htmlCanvasElement) {
+    var canvasRenderingContext2D = htmlCanvasElement.getContext('2d');
     if (canvasRenderingContext2D instanceof CanvasRenderingContext2D) {
         return canvasRenderingContext2D;
     }
     throw new Error('Could not get CanvasRenderingContext2D.');
 }
+exports.getCanvasRenderingContext2D = getCanvasRenderingContext2D;
--- GameOfLife/scripts/javascripts/main.js
+++ GameOfLife/scripts/javascripts/main.js
@@ -1,21 +1,23 @@
-import { getHTMLCanvasElementById, getCanvasRenderingContext2D } from "./Helper.js";
-const CANVAS_BOARD = getHTMLCanvasElementById('canvas-board');
-const CONTEXT_BOARD = getCanvasRenderingContext2D(CANVAS_BOARD);
-const CANVAS_SIZE = 800;
+"use strict";
+exports.__esModule = true;
+var Helper_js_1 = require("./Helper.js");
+var CANVAS_BOARD = Helper_js_1.getHTMLCanvasElementById('canvas-board');
+var CONTEXT_BOARD = Helper_js_1.getCanvasRenderingContext2D(CANVAS_BOARD);
+var CANVAS_SIZE = 800;
 CANVAS_BOARD.width = CANVAS_SIZE;
 CANVAS_BOARD.height = CANVAS_SIZE;
-const GRID_SIZE = 50;
-const GRID_GAP_SIZE = CANVAS_SIZE / GRID_SIZE;
+var GRID_SIZE = 50;
+var GRID_GAP_SIZE = CANVAS_SIZE / GRID_SIZE;
 CANVAS_BOARD.addEventListener('click', clickEvent);
 function clickEvent(event) {
-    const rect = this.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
+    var rect = this.getBoundingClientRect();
+    var x = event.clientX - rect.left;
+    var y = event.clientY - rect.top;
     console.log(x, y);
 }
 function drawGrid() {
     CONTEXT_BOARD.beginPath();
-    for (let i = 0; i <= 50; i++) {
+    for (var i = 0; i <= 50; i++) {
         CONTEXT_BOARD.moveTo(i * GRID_GAP_SIZE, 0);
         CONTEXT_BOARD.lineTo(i * GRID_GAP_SIZE, CANVAS_SIZE);
         CONTEXT_BOARD.moveTo(0, i * GRID_GAP_SIZE);
@@ -25,10 +27,10 @@ function drawGrid() {
     CONTEXT_BOARD.stroke();
 }
 drawGrid();
-const board = [];
-for (let i = 0; i < 50; i++) {
+var board = [];
+for (var i = 0; i < 50; i++) {
     board.push([]);
-    for (let j = 0; j < 50; j++) {
+    for (var j = 0; j < 50; j++) {
         board[i].push(0);
     }
 }

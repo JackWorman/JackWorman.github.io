c4e580bbeeb54254c897a895fbd7c36042e9b784
JackWorman <jackworman@gmail.com>
2020-09-20 18:25:04 -0500

    asdf

--- ThreeD/scripts/typescripts/main.ts
+++ ThreeD/scripts/typescripts/main.ts
@@ -11,9 +11,9 @@ const frames: Array<number> = [];
 
 window.addEventListener('load', () => {
     resizeCanvas();
-    for (var i = 0; i < 1; i++) {
-        // const pO = new PhysicsObject({x: 0, y: 0, z: 0}, {x: 0, y: 0, z: 0}, {x: 0, y: 0, z: 0}, {x: Math.random() - 0.5, y: Math.random() - 0.5, z: (Math.random() - 0.5)});
-        const pO = new PhysicsObject({x: 0, y: 0, z: 0}, {x: 0, y: 0, z: 0}, {x: 0, y: 0, z: 0}, {x: Math.random() - 0.5, y: 0, z: 0});
+    for (var i = 0; i < 5000; i++) {
+        const pO = new PhysicsObject({x: 0, y: 0, z: 0}, {x: 0, y: 0, z: 0}, {x: 0, y: 0, z: 0}, {x: Math.random() - 0.5, y: Math.random() - 0.5, z: (Math.random() - 0.5)});
+        // const pO = new PhysicsObject({x: 0, y: 0, z: 0}, {x: 0, y: 0, z: 0}, {x: 0, y: 0, z: 0}, {x: Math.random() - 0.5, y: 0, z: 0});
         const dot = new Dot(pO, 10);
         dots.push(dot);
     }
